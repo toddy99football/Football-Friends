7172ec53c4d39dd9ecba7812b9f0611ea3b579fa
toddy99football <toddy99football@googlemail.com>
2026-05-14 21:29:06 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -16,4 +16,4 @@ export default async function handler(req, res) {
 
   const data = await response.json();
   res.status(response.status).json(data);
-}
+};
